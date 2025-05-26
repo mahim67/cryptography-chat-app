2fd26df47985b166030f06fbb5810088643e4b51
Rabbi Ahamed <rabbiahamed728@gmail.com>
2025-05-27 00:07:59 +0600

    update

--- app/_app.jsx
+++ app/_app.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Sidebar from './components/sidebar';
 import ChatList from './components/chat-list';
 import { usePathname } from "next/navigation";
+import AuthGate from "./AuthGate";
 
 const AppLayout = ({ children }) => {
     const [selectedUser, setSelectedUser] = useState(null);
@@ -17,28 +18,19 @@ const AppLayout = ({ children }) => {
         )
     }
     return (
-        <div className="flex h-screen">
-            <Sidebar />
-            <div className="flex flex-1">
-                <div className="border-r">
-                    <ChatList onSelectUser={setSelectedUser} />
-                </div>
-                <div className="flex-1">
-                    {/* {selectedUser ? (
-                        <div className="flex flex-col h-full">
-                            <div className="flex-1 overflow-auto">
-                                <ChatInbox selectedUser={selectedUser} />
-                            </div>
-                        </div>
-                    ) : (
-                        <div className="flex items-center justify-center h-full">
-                            <p className="text-gray-500">Select a user to start chatting</p>
-                        </div>
-                    )} */}
-                    {children}
+        <AuthGate>
+            <div className="flex h-screen">
+                <Sidebar />
+                <div className="flex flex-1">
+                    <div className="border-r">
+                        <ChatList onSelectUser={setSelectedUser} />
+                    </div>
+                    <div className="flex-1">
+                        {children}
+                    </div>
                 </div>
             </div>
-        </div>
+        </AuthGate>
     );
 }
 
